2becc350ab99f4422dfa153b90ae350bef79bd6b
Geoffroy Aubry <geoffroy.aubry@twenga.com>
2011-05-17 11:47:53 +0200

    Fondations ok.
    Change-Id: Ic23e8e09788097b5c962fdf0dcee84dab38caa31

---
+++ conf/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+define('CLI_ROOT_DIR', __DIR__ . '/..');
+define('CLI_CONF_DIR', CLI_ROOT_DIR . '/conf');
+define('CLI_INC_DIR', CLI_ROOT_DIR . '/inc');
+define('CLI_SCRIPTS_DIR', CLI_ROOT_DIR . '/scripts');
+define('CLI_SHELL_SCRIPTS_DIR', CLI_SCRIPTS_DIR . '/shell');
+define('CLI_PHP_SCRIPTS_DIR', CLI_SCRIPTS_DIR . '/php');
+define('CLI_LOG_DIR', CLI_ROOT_DIR . '/logs');
+define('CLI_DATA_DIR', CLI_ROOT_DIR . '/data');
+define('CLI_BACKUPS_DIR', CLI_ROOT_DIR . '/backups');
