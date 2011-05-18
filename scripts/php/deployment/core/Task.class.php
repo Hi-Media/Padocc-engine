a98c8b4af7e028bb0d30e66ee1a4054043c514ee
geoffroy.aubry@twenga.com <geoffroy.aubry@twenga.com>
2011-05-18 23:22:09 +0200

    PHPDoc

--- scripts/php/deployment/core/Task.class.php
+++ scripts/php/deployment/core/Task.class.php
@@ -2,8 +2,18 @@
 
 abstract class Task {
 
+	/**
+	 * Compteur d'instances pour s'y retrouver dans les backups des tâches.
+	 * @var int
+	 * @see $sBackupDir
+	 */
 	private static $iCounter = 0;
 
+	/**
+	 * Retourne le nom du tag XML correspondant à cette tâche dans les config projet.
+	 *
+	 * @return string nom du tag XML correspondant à cette tâche dans les config projet.
+	 */
 	public static function getTagName () {
 		throw new RuntimeException('Unimplemented!');
 	}
@@ -16,6 +26,7 @@ abstract class Task {
 
 	/**
 	 * Attributs XML de la tâche.
+	 * Tableau ((string) clé, (string) valeur).
 	 * @var array
 	 */
 	protected $aAttributes;
@@ -26,11 +37,11 @@ abstract class Task {
 		$this->oTask = $oTask;
 		$this->sBackupDir = $sBackupDir . '/' . (++self::$iCounter) . '_' . get_class($this);
 
+		// Récupération des attributs :
 		$this->aAttributes = array();
 		foreach ($this->oTask->attributes() as $key => $val) {
 			$this->aAttributes[$key] = (string)$val;
 		}
-		//print_r($this->aAttributes);
 
 		$this->_check();
 	}
