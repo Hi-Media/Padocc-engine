c63919bc7f54081153a13ce52f668118b3e84958
Geoffroy Aubry <geoffroy.aubry@twenga.com>
2011-06-09 20:22:43 +0200

    Embryon de test.
    Change-Id: I427ec07ba1f6775556d61f478da9f571d6ff0bed

---
+++ scripts/php/deployment/tests/core/task/base/CopyTest.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class CopyTest extends PHPUnit_Framework_TestCase {
+    public function testPushAndPop () {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+
+    public static function testToto () {
+    	$stack = array();
+        $this->assertEquals(0, count($stack));
+    }
+}
