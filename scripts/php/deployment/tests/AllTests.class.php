2318810d2fcf2b6db200ba4c52c9294442e0c822
geoffroy.aubry@twenga.com <geoffroy.aubry@twenga.com>
2011-06-13 23:52:35 +0200

    Evo mise en place tests unitaires
    Change-Id: I6b6b6db704bf6d5e68b4a8cf527ce890c442bc73

--- scripts/php/deployment/tests/AllTests.class.php
+++ scripts/php/deployment/tests/AllTests.class.php
@@ -12,14 +12,50 @@ spl_autoload_register(function($sClass) {
 	include_once($sPath);
 });
 
-echo "XXX";
-include_once(DEPLOYMENT_TESTS_DIR . '/core/task/base/CopyTest.class.php');
-
 class AllTests {
 
 	public static function suite() {
-		$suite = new PHPUnit_Framework_TestSuite('YYYPHPUnit');
-		$suite->addTestSuite('CopyTest');
+		$files = self::getFiles(DEPLOYMENT_TESTS_DIR, '*Test.class.php');
+		//var_dump($files);
+		$suite = new PHPUnit_Framework_TestSuite('Deployment');
+		foreach ($files as $file) {
+			include_once($file);
+			$class = basename($file, '.class.php');
+			$suite->addTestSuite($class);
+		}
 		return $suite;
+	}
+
+	/**
+	 * Recursive version of glob.
+	 *
+	 * @param string $path Directory to start with.
+	 * @param mixed $patterns Pattern to glob for, or an array of patterns.
+	 * @return array containing all pattern-matched files.
+	 */
+	public static function getFiles ($path, $patterns) {
+		if ($path{strlen($path) - 1} == '/')
+			$path = substr($path, 0, -1);
+		$path = escapeshellcmd($path);
+
+		// Get the list of all matching files currently in the directory.
+		if ( ! is_array($patterns))
+			$patterns = array($patterns);
+		$files = array();
+		foreach ($patterns as $pattern) {
+			$files = array_merge($files, glob($path.'/'.$pattern, 0));
+		}
+
+		// Then get a list of all directories in this directory, and
+		// run ourselves on the resulting array.  This is the
+		// recursion step, which will not execute if there are no
+		// directories.
+		$paths = glob($path.'/*', GLOB_ONLYDIR|GLOB_NOSORT);
+		foreach ($paths as $path) {
+			$sub_files = self::getFiles($path, $patterns, 0);
+			$files = array_merge($files, $sub_files);
+		}
+
+		return $files;
 	}
 }
---
+++ scripts/php/deployment/tests/core/ShellTest.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class ShellTest extends PHPUnit_Framework_TestCase {
+    public function testXXX () {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+
+		$sMockShell = $this->getMockClass('Shell', array('exec'));
+
+		$sMockShell::staticExpects($this->exactly(2))->method('exec');
+		$sMockShell::staticExpects($this->at(0))->method('exec')->with($this->equalTo('mkdir -p "dest"'));
+		$sMockShell::staticExpects($this->at(1))->method('exec')->with($this->equalTo('cp -ar "src" "dest"'));
+		$result = $sMockShell::copy('src', 'dest');
+		//echo "Result= $result.";
+    }
+
+    /*public static function testToto () {
+    	$stack = array();
+        $this->assertEquals(0, count($stack));
+    }*/
+}
