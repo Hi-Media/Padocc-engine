2645a0d05111e3776fb03bf21d22e15c05aca4bf
geoffroy.aubry@twenga.com <geoffroy.aubry@twenga.com>
2011-06-16 00:56:25 +0200

    Evo gitexport.
    Change-Id: I3ae8c3dc546d22a2bab58518d6b2b511025e3393

--- scripts/php/deployment/core/task/base/Gitexport.class.php
+++ scripts/php/deployment/core/task/base/Gitexport.class.php
@@ -15,25 +15,38 @@ class Task_Base_Gitexport extends Task {
 		parent::__construct($oTask, $oProject, $sBackupPath);
 		$this->aAttributeProperties = array(
 			'repository' => array('file', 'required'),
-			'branch' => array('dir', 'required'),
+			'branch' => array('required'),
+			//'tag' => array(),
+			'srcdir' => array('dir', 'required'),
 			'destdir' => array('dir', 'required')
 		);
 	}
 
 	public function check () {
 		parent::check();
+		/*if ( ! empty($this->aAttributes['branch']) XOR ! empty($this->aAttributes['tag'])) {
+			throw new Exception("Attributes 'branch' and 'tag' are exclusive and one of them must be filled!");
+		}*/
 	}
 
 	public function execute () {
-		Shell::copy($this->aAttributes['src'], $this->aAttributes['destdir']);
+		//$ref = ( ! empty($this->aAttributes['branch']) ? 'origin/' . $this->aAttributes['branch'] : $this->aAttributes['tag']);
+		$result = Shell::exec(
+			DEPLOYMENT_BASH_PATH . ' ' . DEPLOYMENT_INC_DIR . '/gitexport.inc.sh'
+			. ' "' . $this->aAttributes['repository'] . '"'
+			. ' "' . $this->aAttributes['branch'] . '"'
+			. ' "' . $this->aAttributes['srcdir'] . '"'
+			. ' "' . $this->aAttributes['destdir'] . '"'
+		);
+		var_dump(implode("\n", $result));
 	}
 
 	public function backup () {
-		if (Shell::getFileStatus($this->aAttributes['destdir']) !== 0) {
+		/*if (Shell::getFileStatus($this->aAttributes['destdir']) !== 0) {
 			list($bIsRemote, $aMatches) = Shell::isRemotePath($this->aAttributes['destdir']);
 			$sBackupPath = ($bIsRemote ? $aMatches[1]. ':' : '') . $this->sBackupPath . '/'
 				. pathinfo($aMatches[2], PATHINFO_BASENAME) . '.tar.gz';
 			Shell::backup($this->aAttributes['destdir'], $sBackupPath);
-		}
+		}*/
 	}
 }
