c109ca7f96edb773b18db83e22cd7f9101ddf322
Geoffroy Aubry <geoffroy.aubry@twenga.com>
2011-09-07 19:28:35 +0200

    1er jet Minifier

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -10,6 +10,7 @@ define('DEPLOYMENT_RESOURCES_DIR', DEPLOYMENT_ROOT_DIR . '/resources');
 define('DEPLOYMENT_REPOSITORIES_DIR', '$HOME/deployment_repositories');
 define('DEPLOYMENT_DEBUG_MODE', 1);	// 1 ou 0
 define('DEPLOYMENT_BASH_PATH', '/bin/bash');
+define('DEPLOYMENT_JSMIN_BIN_PATH', DEPLOYMENT_LIB_DIR . '/minifier/jsmin/jsmin');
 
 /**
  * Chemin du répertoire temporaire système utilisable par l'application.
