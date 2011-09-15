60ae5b427c3dae96be0aa66a2dd5c3cffd0d1e88
Geoffroy Aubry <geoffroy.aubry@twenga.com>
2011-09-15 10:34:04 +0200

    ADD classe Tools d'outils inclassables + tests unitaires

---
+++ lib/Tools.class.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Classe outil des inclassables.
+ *
+ * @category TwengaDeploy
+ * @package Lib
+ * @author Geoffroy AUBRY <geoffroy.aubry@twenga.com>
+ */
+class Tools
+{
+
+    /**
+     * Retourne un couple comprenant d'une part le nombre d'octets contenus dans la plus grande unité informatique
+     * inférieure à la taille spécifiée, et d'autre part le nom de cette unité.
+     *
+     * Par exemple, si $iFileSize vaut 2000, alors le résultat sera : array(1024, 'Kio').
+     *
+     * @param int $iFileSize taille en octets à changer d'unité
+     * @return array tableau (int, string) comprenant d'une part le nombre d'octets contenus dans la plus grande unité inférieure
+     * à la taille spécifiée, et d'autre part le nom de cette unité.
+     */
+    public static function getFileSizeUnit ($iFileSize)
+    {
+        if ($iFileSize < 1024) {
+            $iUnit = 1;
+            $sUnit = 'o';
+        } else if ($iFileSize < 1024*1024) {
+            $iUnit = 1024;
+            $sUnit = 'Kio';
+        } else {
+            $iUnit = 1024*1024;
+            $sUnit = 'Mio';
+        }
+        return array($iUnit, $sUnit);
+    }
+
+    /**
+     * Retourne un couple comprenant d'une part la taille spécifiée arrondie,
+     * et d'autre part l'unité dans laquelle la taille a été arrondie.
+     *
+     * Le second paramètre, si <> de 0, permet de spécifier une taille de référence pour le calcul de l'unité.
+     *
+     * Par exemple :
+     * (100, 0) => ('100', 'o')
+     * (100, 2000000) => ('<1', 'Mio')
+     * (200, 0) => ('2', 'Kio')
+     *
+     * @param int $iSize taille à convertir
+     * @param int $iRefSize référentiel de conversion, si différent de 0
+     * @return array un couple comprenant d'une part la taille spécifiée arrondie,
+     * et d'autre part l'unité dans laquelle la taille a été arrondie.
+     */
+    public static function convertFileSize2String ($iSize, $iRefSize=0)
+    {
+        if ($iRefSize === 0) {
+            $iRefSize = $iSize;
+        }
+        list($iUnit, $sUnit) = self::getFileSizeUnit($iRefSize);
+
+        $sFileSize = round($iSize/$iUnit);
+        if ($sFileSize == 0 && $iSize > 0) {
+            $sFileSize = '<1';
+        }
+        return array($sFileSize, $sUnit);
+    }
+
+    /**
+     * Classe outil non instanciable.
+     */
+    private function __construct ()
+    {
+    }
+}
