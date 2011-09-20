e7ed756f3f816167e4816266b9f2d90709c4d9b2
Geoffroy Aubry <geoffroy.aubry@twenga.com>
2011-09-20 09:49:13 +0200

    mineur

--- tests/core/task/base/resources/config_template.inc.php
+++ tests/core/task/base/resources/config_template.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 define('PROJECT', '${PROJECT_NAME}');
+define('PROJECT_BIS', '${PROJECT_NAME}');
 define('ENV', '${ENVIRONMENT_NAME}');
 define('EXECUTION_ID', '${EXECUTION_ID}');
 define('TEST', '${NOT_EXISTS}');
