4e18247ce170b49ed1479e9d7aa9ed6983aaf6b6
gaubry <geoffroy.aubry@twenga.com>
2011-10-26 16:22:59 +0200

    ADD PHP Doc

--- tests/core/task/base/resources/config_template.inc.php
+++ tests/core/task/base/resources/config_template.inc.php
@@ -1,9 +1,43 @@
 <?php
 
+/**
+ * bla bla...
+ * @var string
+ */
 define('PROJECT', '${PROJECT_NAME}');
+
+/**
+ * bla bla...
+ * @var string
+ */
 define('PROJECT_BIS', '${PROJECT_NAME}');
+
+/**
+ * bla bla...
+ * @var string
+ */
 define('ENV', '${ENVIRONMENT_NAME}');
+
+/**
+ * bla bla...
+ * @var string
+ */
 define('EXECUTION_ID', '${EXECUTION_ID}');
+
+/**
+ * bla bla...
+ * @var string
+ */
 define('BASEDIR', '${BASEDIR}');
+
+/**
+ * bla bla...
+ * @var string
+ */
 define('LOCAL_PROPERTY_SERVERS', '${SERVERS}');
+
+/**
+ * bla bla...
+ * @var string
+ */
 define('TEST', '${NOT_EXISTS}');
