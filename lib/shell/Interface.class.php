0a061aaee7efc3c40984a6c931f7049993f94444
gaubry <geoffroy.aubry@twenga.com>
2011-12-20 17:20:41 +0100

    FIX oubli des self::$_aSmartyRsyncExclude à exclure lors des rsync de Task_Base_Environment::_initNewRelease() suite à la nouvelle approche de parallélisation.

--- lib/shell/Interface.class.php
+++ lib/shell/Interface.class.php
@@ -203,13 +203,17 @@ interface Shell_Interface
      *
      * @param string $sSrcPath au format [[user@]hostname_or_ip:]/path
      * @param string|array $mDestPath chaque destination au format [[user@]hostname_or_ip:]/path
+     * @param array $aValues liste de valeurs (string) optionnelles pour générer autant de demande de
+     * synchronisation en parallèle. Dans ce cas ces valeurs viendront remplacer l'une après l'autre
+     * les occurences de crochets vide '[]' présents dans $sSrcPath ou $sDestPath.
      * @param array $aIncludedPaths chemins à transmettre aux paramètres --include de la commande shell rsync.
      * Il précéderons les paramètres --exclude.
      * @param array $aExcludedPaths chemins à transmettre aux paramètres --exclude de la commande shell rsync
      * @return array tableau indexé du flux de sortie shell des commandes rsync exécutées,
      * découpé par ligne et analysé par _resumeSyncResult()
      * @throws RuntimeException en cas d'erreur shell
-     * @throws RuntimeException car non implémenté quand plusieurs $mDestPath et $sSrcPath est distant
+     * @throws RuntimeException car non implémenté quand plusieurs $mDestPath et $sSrcPath sont distants
      */
-    public function sync ($sSrcPath, $mDestPath, array $aIncludedPaths=array(), array $aExcludedPaths=array());
+    public function sync ($sSrcPath, $sDestPath, array $aValues=array(),
+            array $aIncludedPaths=array(), array $aExcludedPaths=array());
 }
