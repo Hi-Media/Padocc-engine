77374f41f20356b33e6e374dd86558417887a99f
Tony Caron <tony.caron@twenga.com>
2012-11-28 18:20:19 +0100

    Add real key => value system for property

--- fuel/app/modules/deploy_core/task/base/ExternalProperty.class.php
+++ fuel/app/modules/deploy_core/task/base/ExternalProperty.class.php
@@ -1,4 +1,5 @@
 <?php
+namespace Fuel\Tasks;
 
 /**
  * Définit une propriété externe qu'il sera obligatoire de fournir lors de tout déploiement.
@@ -14,27 +15,7 @@
 class Task_Base_ExternalProperty extends Task
 {
 
-    /**
-     * Préfixe de propriété externe, c.-à-d. fournie par l'utilisateur.
-     * @var string
-     * @see _centralExecute()
-     */
-    const EXTERNAL_PROPERTY_PREFIX = 'external_property_';
-
-    /**
-     * Compteur général du nombre de propriétés externes résolues, c.-à-d. associées à une variable
-     * du fichier de configuration XML (les '${my_var}').
-     * @var int
-     */
-    private static $_iCounter = 0;
-
-    /**
-     * Numéro de cette instance de propriété externe : c'est la valeur de self::$_iCounter à la création.
-     * La première instance vaudra donc 1.
-     * @var int
-     */
-    private $_iNumber;
-
+    
     /**
      * Retourne le nom du tag XML correspondant à cette tâche dans les config projet.
      *
@@ -52,7 +33,7 @@ class Task_Base_ExternalProperty extends Task
      * @param Task_Base_Project $oProject Super tâche projet.
      * @param ServiceContainer $oServiceContainer Register de services prédéfinis (Shell_Interface, ...).
      */
-    public function __construct (SimpleXMLElement $oTask, Task_Base_Project $oProject,
+    public function __construct (\SimpleXMLElement $oTask, Task_Base_Project $oProject,
         ServiceContainer $oServiceContainer)
     {
         parent::__construct($oTask, $oProject, $oServiceContainer);
@@ -60,7 +41,6 @@ class Task_Base_ExternalProperty extends Task
             'name' => AttributeProperties::REQUIRED,
             'description' => AttributeProperties::REQUIRED
         );
-        $this->_iNumber = ++self::$_iCounter;
     }
 
     /**
@@ -73,11 +53,12 @@ class Task_Base_ExternalProperty extends Task
     {
         parent::_centralExecute();
         $this->_oLogger->indent();
+        var_dump($this->_aAttributes);
         try {
-            $sValue = $this->_oProperties->getProperty(self::EXTERNAL_PROPERTY_PREFIX . $this->_iNumber);
-        } catch (UnexpectedValueException $oException) {
+            $sValue = $this->_oProperties->getProperty($this->_aAttributes['name']);
+        } catch (\UnexpectedValueException $oException) {
             $sMsg = "Property '" . $this->_aAttributes['name'] . "' undefined!";
-            throw new UnexpectedValueException($sMsg, 1, $oException);
+            throw new \UnexpectedValueException($sMsg, 1, $oException);
         }
         $sMsg = "Set external property '" . $this->_aAttributes['name'] . "' (description: '"
               . $this->_aAttributes['description'] . "') to '$sValue'.";
