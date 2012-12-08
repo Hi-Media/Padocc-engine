dad979bb39101941a1c16726140b9c67cc3de07f
Tony Caron <tony.caron@twenga.com>
2012-12-08 05:27:03 +0100

    Add comment

--- fuel/app/classes/model/project.php
+++ fuel/app/classes/model/project.php
@@ -1,19 +1,33 @@
 <?
+/**
+ * Management of projects
+ * @category Model
+ * @package Tony CARON <caron.tony@gmail.com>
+ */
 
 namespace Model;
 use DB;
 
 class Project extends \Model {
 
-    public static function add($sProjectName, $sGroup, $iOwnerID, $iCreatorId)
+	/**
+     * Add a new project
+     *
+     * @param string $sProjectName Project name
+     * @param string $sProjectGroup Project group
+     * @param int $iOwnerID UserId owner
+     * @param int $iCreatorId UserId creator
+     * @return array 
+     */
+    public static function add($sProjectName, $sProjectGroup, $iOwnerID, $iCreatorId)
     {
         // prepare an insert statement
-		$sQuery = DB::insert('DEE_PROJECT');
+		$sQuery = DB::insert('EDE_PROJECT');
 
 		// Set the columns and vales
 		$aReturn = $sQuery->set(array(
 		    'NAME' => $sProjectName,
-		    'GROUP' => $sGroup,
+		    'GROUP' => $sProjectGroup,
 		    'OWNER_ID' => $iOwnerID,
 		    'CREATOR_ID' => $iCreatorId,
 		    'DATE_INSERT' => DB::expr('NOW()')
@@ -22,10 +36,17 @@ class Project extends \Model {
 		return $aReturn[0];
     }
 
+    /**
+     * Update a project
+     *
+     * @param int $iProjectId Project ident
+     * @param string $sGroup Project group
+     * @param int $iOwnerID UserId owner
+     */
     public static function update($iProjectId, $sProjectGroup, $iOwnerId)
     {
         // prepare an insert statement
-		$sQuery = DB::UPDATE('DEE_PROJECT');
+		$sQuery = DB::UPDATE('EDE_PROJECT');
 
 		// Set the columns and vales
 		$sQuery->set(array(
@@ -36,18 +57,34 @@ class Project extends \Model {
     	->execute();
     }
 
+    /**
+     * Return if a project's name already exists
+     *
+     * @param string $sProjectName Project name
+     * @param string $sProjectGroup Project group
+     * @param int $iOwnerID UserId owner
+     * @param int $iCreatorId UserId creator
+     * @return bool 
+     */
 	public static function exist($sProjectName)
 	{
 
-		$aResult = DB::query('SELECT NAME FROM `DEE_PROJECT` WHERE NAME = :sProjectName ')
+		$aResult = DB::query('SELECT NAME FROM `EDE_PROJECT` WHERE NAME = :sProjectName ')
 		->bind('sProjectName', $sProjectName)->execute();
 
 		return count($aResult) ? true : false;
 	}
 
+
+    /**
+     * Return project's datas
+     *
+     * @param int $iProjectId optional Project Ident
+     * @return array of projects data 
+     */
 	public static function listing($iProjectId = NULL)
 	{
-		$sQuery = 'SELECT * FROM `DEE_PROJECT` ';
+		$sQuery = 'SELECT * FROM `EDE_PROJECT` ';
 
 		if(!NULL == $iProjectId)
 		{
@@ -58,16 +95,22 @@ class Project extends \Model {
 		return DB::query($sQuery.' ORDER BY NAME ASC')->bind('iProjectId', $iProjectId)->execute()->as_array();
 	}
 
+	/**
+     * Return extended project's datas
+     *
+     * @param int $iProjectId optional Project Ident
+     * @return array of extended projects data 
+     */
 	public static function listingEx($iProjectId = NULL)
 	{
 		$sQuery = '
 			SELECT *, 
 			du.FIRSTNAME as OWNER_FIRSTNAME, du.LASTNAME as OWNER_LASTNAME , 
 			du2.FIRSTNAME as CREATOR_FIRSTNAME, du2.LASTNAME as CREATOR_LASTNAME  
-			FROM DEE_PROJECT dp
+			FROM EDE_PROJECT dp
 			JOIN DEE_USER du ON du.USER_ID = dp.OWNER_ID
 			JOIN DEE_USER du2 ON du2.USER_ID = dp.CREATOR_ID
-			JOIN DEE_PROJECT_CONFIGURATION dpc on dpc.PROJECT_ID = dp.PROJECT_ID AND dpc.STATUS = "ACTIVE"
+			JOIN EDE_PROJECT_CONFIGURATION dpc on dpc.PROJECT_ID = dp.PROJECT_ID AND dpc.STATUS = "ACTIVE"
 			 ';
 
 		$sOrder = ' ORDER BY dp.NAME ASC';
@@ -81,15 +124,21 @@ class Project extends \Model {
 		return DB::query($sQuery.$sOrder)->execute()->as_array();
 	}
 
+
 	public static function listingByGroup()
 	{
-		return DB::query('SELECT * FROM `DEE_PROJECT` ORDER BY `GROUP` ASC, NAME ASC; ')
+		return DB::query('SELECT * FROM `EDE_PROJECT` ORDER BY `GROUP` ASC, NAME ASC; ')
 		->execute()->as_array();
 	}
 
+	/**
+     * Return all groups
+     *
+     * @return array of groups names
+     */
 	public static function listingGroup()
 	{
-		return DB::query('SELECT distinct(`GROUP`) FROM `DEE_PROJECT` ')
+		return DB::query('SELECT distinct(`GROUP`) FROM `EDE_PROJECT` ')
 		->execute()->as_array();
 	}
 
--- fuel/app/classes/model/user.php
+++ fuel/app/classes/model/user.php
@@ -1,14 +1,27 @@
 <?
+/**
+ * Management of users
+ * @category Model
+ * @package Tony CARON <caron.tony@gmail.com>
+ */
 
 namespace Model;
 use DB;
 use Session;
 class User extends \Model {
 
+    /**
+     * Add a new user
+     *
+     * @param string $sFirstName First name
+     * @param string $sLastname Last name
+     * @param string $sEmail User's email
+     * @return array 
+     */
     public static function add($sFirstName, $sLastname, $sEmail)
     {
         // prepare an insert statement
-		$sQuery = DB::insert('DEE_USER');
+		$sQuery = DB::insert('EDE_USER');
 
 		// Set the columns and vales
 		$aReturn = $sQuery->set(array(
@@ -21,31 +34,50 @@ class User extends \Model {
 		return $aReturn[0];
     }
 
+    /**
+     * Return all users
+     *
+     * @return array of users
+     */
     public static function listing()
     {
-    	$aResult = DB::query('SELECT * FROM `DEE_USER` ORDER BY FIRSTNAME ASC')
+    	$aResult = DB::query('SELECT * FROM `EDE_USER` ORDER BY FIRSTNAME ASC')
 		->execute();
 
 		return $aResult->as_array();
     }
 
+    /**
+     * Returen if a uers is logged
+     *    
+     * @return bool 
+     */
     public static function isLogged()
     {
         return true === $this->session->userdata('LOGGED');
     }
 
-
+    /**
+     * Return current logged user User_Id
+     *
+     * @return int 
+     */
     public static function getLoggedUserId()
     {
          return Session::get('USER_ID');
     }
 
+    /**
+     * Log an user with his mail
+     *
+     * @param string $sEmail User's mail
+     */
     public static function login($sEmail)
     {
     	Session::set('LOGGED', true);
     	Session::set('USER', $sEmail);
 
-        $aResult = DB::query('SELECT * FROM DEE_USER WHERE EMAIL = "'.$sEmail.'"')->execute()->as_array();
+        $aResult = DB::query('SELECT * FROM EDE_USER WHERE EMAIL = "'.$sEmail.'"')->execute()->as_array();
 
         Session::set('USER_ID', $aResult[0]['USER_ID']);
 
@@ -55,7 +87,7 @@ class User extends \Model {
    /* public static function update($iProjectId, $sProjectName, $sGroup)
     {
         // prepare an insert statement
-		$sQuery = DB::UPDATE('DEE_PROJECT');
+		$sQuery = DB::UPDATE('EDE_PROJECT');
 
 		// Set the columns and vales
 		$sQuery->set(array(
@@ -66,10 +98,15 @@ class User extends \Model {
     	->execute();
     }*/
 
+    /**
+     * Return if an user exists 
+     *
+     * @param string $sEmail User's mail
+     */
 	public static function exist($sEmail)
 	{
 
-		$aResult = DB::query('SELECT EMAIL FROM `DEE_USER` WHERE EMAIL = :sEmail ')
+		$aResult = DB::query('SELECT EMAIL FROM `EDE_USER` WHERE EMAIL = :sEmail ')
 		->bind('sEmail', $sEmail)->execute();
 
 		return count($aResult) ? true : false;
