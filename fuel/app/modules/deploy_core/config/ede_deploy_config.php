a2ba7c024fa1e581fb5307d51089249c9fc1412d
Tony Caron <tony.caron@twenga.com>
2012-12-09 04:16:05 +0100

    Tmp config file

---
+++ fuel/app/modules/deploy_core/config/ede_deploy_config.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+
+	
+	
+	/**
+	 * Paths conf
+	 */
+	'paths' => array(
+		/**
+		 * Chemin du répertoire temporaire système utilisable par l'application.
+		 * @var string
+		 */
+		'deployment_tmp_dir' => '/tmp',
+		
+	),
+/*
+	'bin' => array(
+		'php' => "auto"
+	),*/
+
+);
+
+
+
