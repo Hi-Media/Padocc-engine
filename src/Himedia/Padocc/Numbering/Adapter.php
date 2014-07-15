df317c8b490ed12d0d7d65ca42f2d72b7152c252
Geoffroy Aubry <gaubry@hi-media.com>
2014-07-15 22:31:05 +0200

    Add Apache License, Version 2.0

--- src/Himedia/Padocc/Numbering/Adapter.php
+++ src/Himedia/Padocc/Numbering/Adapter.php
@@ -9,7 +9,25 @@ namespace Himedia\Padocc\Numbering;
  * L'inverse est également vrai si l'on n'est pas au niveau le plus haut.
  * Par exemple : 1.3.7 => 1.3 => 1.3.7
  *
- * @author Geoffroy AUBRY <gaubry@hi-media.com>
+ *
+ *
+ * Copyright (c) 2014 HiMedia Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright 2014 HiMedia Group
+ * @author Geoffroy Aubry <gaubry@hi-media.com>
+ * @license Apache License, Version 2.0
  */
 class Adapter implements NumberingInterface
 {
